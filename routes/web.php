8ae0e276883a43da2550c426ac0e7a2ec54dd7fa
khaleefa13 <muhammaddihyakhalifa@students.amikom.ac.id>
2026-04-10 14:49:15 +0700

    perubahan

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('home');
 });
 
