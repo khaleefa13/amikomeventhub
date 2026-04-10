2fe40345c5447a22fb6579c794563ee249cbc9fe
khaleefa13 <muhammaddihyakhalifa@students.amikom.ac.id>
2026-04-10 08:06:16 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Muhammad Dihya Khalifa 24.12.3272';
+});
+
+Route::get('/tentang', function () {
+return '<h1>Ini adalah Halaman Tentang Aplikasi Event Hub</h1>';
 });
