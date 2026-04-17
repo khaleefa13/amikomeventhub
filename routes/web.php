89714db6fa76fa626441cf158611953b48d2bae4
khaleefa13 <muhammaddihyakhalifa@students.amikom.ac.id>
2026-04-17 08:43:29 +0700

    tugas 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController; 
+use App\Http\Controllers\EventController; 
+use App\Http\Controllers\Admin\DashboardController; 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -31,4 +34,26 @@ Route::get('/katalog', function () {
 // Rute untuk Bantuan
 Route::get('/bantuan', function () {
     return view('bantuan');
+});
+
+
+// Rute User Area
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/event/1', [EventController::class, 'show'])->name('events.show');
+
+Route::get('/checkout', [EventController::class, 'checkout'])->name('checkout');
+
+Route::get('/my-ticket', [EventController::class, 'ticket'])->name('ticket');
+
+
+// Rute Admin Area
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::get('/events', [EventController::class, 'indexAdmin'])->name('events.index');
+
+    // dan seterusnya...
+
 });
